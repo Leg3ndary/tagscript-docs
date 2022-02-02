4ddfdef21550ab64da16775ea26eb377ca2aba31
ben_z_13 <57199957+teddybears123456789@users.noreply.github.com>
2022-02-02 18:50:05 -0500

    Trying this

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'lunar.sphinx.ext.ansi',
+    'sphinx.ext.ansi',
 ]
 
 intersphinx_mapping = {
